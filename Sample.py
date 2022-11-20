b86b766b61fdfba6e48c9758b534ad8582f6f354
Chewbakabra <antonlisovenko123@gmail.com>
2022-11-20 14:45:32 +0400

    autouse fixture added second test with assertion by class error added

--- Sample.py
+++ Sample.py
@@ -4,6 +4,7 @@ from time import sleep
 from selenium.webdriver.common.by import By
 
 # если нужно, то можно задать как autouse или добавить маркер
+
 @pytest.fixture()
 def browser():
     #параметры нужно будет куда-то закинуть отдельно?
@@ -12,12 +13,13 @@ def browser():
     # эта хуета выполнится до теста
     browser = webdriver.Chrome()
     browser.get(base_url)
-    browser.maximize_window()
     yield browser
     # эта хуета выполнится после теста
     sleep(2)
     browser.close()
-
+@pytest.fixture(autouse=True)
+def autouse_example(browser):
+    browser.maximize_window()
 
 def test_text_box_positive(browser):
 
@@ -34,5 +36,12 @@ def test_text_box_positive(browser):
     sleep(5)
 
     assert browser.find_element(By.ID, "output").is_displayed()
-    assert browser.find_element(By.ID, "output").text.__contains__(text)
+    assert text in browser.find_element(By.ID, "output").text
     #  use "in" instead of __contains
+
+def test_text_box_negative(browser):
+    browser.find_element(By.ID, "userEmail").send_keys("invalid")
+    email_error_label = By.XPATH, '//input[@id="userEmail" and contains(@class,"error")]'
+    assert browser.find_element(email_error_label).is_displayed()
+    # запринтить get_attribute class - не понял, какой в этом смысл,
+    # get attribute "class" вернет не error, а в xpath выше он уже прописан
---
+++ education/lists.py
@@ -0,0 +1,11 @@
+#lists
+
+l = ["str", 123, 1.2, ["another", 321]]
+print(l)
+
+a = list("example")
+print(a)
+
+print(a[0:3])
+
+print(a[0:6])
